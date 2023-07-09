47159707e39037f1311ec3cf2c9d93528c7529d1
pheeling <philipph@vitalitystream.ch>
2023-07-10 01:00:21 +0200

    feat(common):included contractWriteForm

---
+++ bccdao/app/page.js
@@ -0,0 +1,116 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+import { WagmiConfig, createConfig, mainnet } from 'wagmi'
+import { createPublicClient, http } from 'viem'
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { ProposeForm } from '/components/contractFunctions'
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient: createPublicClient({
+    chain: mainnet,
+    transport: http()
+  }),
+})
+
+function Profile() {
+  const { address, isConnected } = useAccount()
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  })
+  const { disconnect } = useDisconnect()
+
+  if (isConnected)
+    return (
+      <div>
+        Connected to {address}
+        <button onClick={() => disconnect()}>Disconnect</button>
+        <ProposeForm />
+      </div>
+    )
+  return <button onClick={() => connect()}>Connect Wallet</button>
+}
+
+// Pass config to React Context Provider
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+        <code className={styles.code}>Get started by making your first proposal&nbsp;</code>
+        </p>
+        <div>
+          <a
+            href="https://uzh.ch/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/uzh_logo_schwarz.svg"
+              alt="UZH Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/BCC-logo-sw.png"
+          alt="BCC Logo"
+          width={425}
+          height={400}
+          priority
+        />
+        <div>
+          <Profile />
+        </div>
+      </div>
+
+      <div className={styles.grid3}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Vote <span>-&gt;</span>
+          </h2>
+          <p>Vote on proposals</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Make a proposal <span>-&gt;</span>
+          </h2>
+          <p>Share and create</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            List proposals<span>-&gt;</span>
+          </h2>
+          <p>Explore BCC DAO</p>
+        </a>
+      </div>
+    </main>
+  )
+}
